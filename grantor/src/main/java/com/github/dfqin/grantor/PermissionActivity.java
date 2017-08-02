f0a61161eebcad84c2505aeb3a721bc3e85e40cf
 “qindongfang” <dfqin@hotmail.com>
2017-08-02 16:22:02 +0800

    modify dialog style

--- grantor/src/main/java/com/github/dfqin/grantor/PermissionActivity.java
+++ grantor/src/main/java/com/github/dfqin/grantor/PermissionActivity.java
@@ -5,17 +5,18 @@ package com.github.dfqin.grantor;
  */
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
 import java.io.Serializable;
 
-public class PermissionActivity extends Activity {
+public class PermissionActivity extends AppCompatActivity {
 
 
     private static final int PERMISSION_REQUEST_CODE = 64;
